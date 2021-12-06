0817545fe1885681f26fd593df0d4578cab89f3e
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2021-12-06 13:01:13 +0530

    Updated views for home page rendering to fetch related images for the post from relation Image model as well

--- anonymous_social_question_answer/views.py
+++ anonymous_social_question_answer/views.py
@@ -2,14 +2,26 @@ from django.contrib.auth.decorators import login_required
 from users.models import CustomUser
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from post.models import Images, Post
 
 @login_required
 def home(request):
   """this views is called when the user hits the route domain/home"""
   try:
     if request.user.is_authenticated and not request.user.is_superuser:
-      data = CustomUser.objects.get(user=request.user) #fetch the user from database
-      return render(request, "home.html", {"data": data}) #send the data to home page as well
+      user_data = CustomUser.objects.get(user=request.user) #fetch the user from database
+      posts = Post.objects.all()[:10]
+      posts_data = []
+      for post in posts:
+        data = {}
+        try:
+          images = Images.objects.filter(post=post)
+        except Images.DoesNotExist:
+          images = []
+        data['images'] = images
+        data['post'] = post
+        posts_data.append(data)
+      return render(request, "home.html", {"user_data": user_data,"posts_data":(posts_data)}) #send the data to home page as well
     else:
       #if the user is unauthenticated
       messages.warning(request, "Only Registered user allowed")
