f8d00ae9cfbfc00c6f6b2faf149e5bd0b5201e44
Akankshi gupta <akankshigp12345@gmail.com>
2022-04-06 14:47:19 +0530

    added method to fetch comment, added method to insert post onclick

--- static/js/home.js
+++ static/js/home.js
@@ -119,12 +119,12 @@ var voteAnswer = function (answer_id) { return __awaiter(_this, void 0, void 0, 
             case 1:
                 response = _a.sent();
                 if (response.message == 'Voted the answer!') {
-                    manipulate("vote-" + answer_id, 'Un-Vote');
-                    updateVoteCount("vote-count-" + answer_id, true);
+                    toggleIcon("answer-" + answer_id, 'fa-check-circle', 'fa-check');
+                    updateVoteCount("answer-vote-count-" + answer_id, true);
                 }
                 else {
-                    manipulate("vote-" + answer_id, 'Vote');
-                    updateVoteCount("vote-count-" + answer_id, false);
+                    toggleIcon("answer-" + answer_id, 'fa-check', 'fa-check-circle');
+                    updateVoteCount("answer-vote-count-" + answer_id, false);
                 }
                 return [3 /*break*/, 3];
             case 2:
@@ -194,8 +194,30 @@ var reportPost = function (post_id) { return __awaiter(_this, void 0, void 0, fu
         }
     });
 }); };
-var reportAccount = function (account_id) { return __awaiter(_this, void 0, void 0, function () {
+var reportAnswer = function (answer_id) { return __awaiter(_this, void 0, void 0, function () {
     var url, response, error_6;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                url = "/api/report_answer/" + answer_id;
+                return [4 /*yield*/, makeRequest(url)];
+            case 1:
+                response = _a.sent();
+                if (response.message == 'Reported!') {
+                    toggleIcon("report-answer-" + answer_id, 'fas', 'far');
+                }
+                return [3 /*break*/, 3];
+            case 2:
+                error_6 = _a.sent();
+                console.error("Error at Report Answer : " + error_6.message);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
+        }
+    });
+}); };
+var reportAccount = function (account_id) { return __awaiter(_this, void 0, void 0, function () {
+    var url, response, error_7;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
@@ -209,8 +231,8 @@ var reportAccount = function (account_id) { return __awaiter(_this, void 0, void
                 }
                 return [3 /*break*/, 3];
             case 2:
-                error_6 = _a.sent();
-                console.error("Error at Report Account : " + error_6.message);
+                error_7 = _a.sent();
+                console.error("Error at Report Account : " + error_7.message);
                 return [3 /*break*/, 3];
             case 3: return [2 /*return*/];
         }
@@ -221,7 +243,7 @@ var convertDateFormat = function (date, id) {
     var time_string = date_obj.toDateString().toString().slice(4) + " at " + date_obj.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     document.getElementById(id).innerHTML = time_string.toString();
 };
-function Validate() {
+var Validate = function () {
     var upload_element = document.getElementById("upload");
     for (var i = 0; i < upload_element.files.length; i++) {
         var filetype = upload_element.files.item(i).type;
@@ -241,7 +263,55 @@ function Validate() {
             alert("Invalid Media");
         }
     }
-}
+};
+var createReplyCard = function (comment, user) {
+    console.log(comment.author.username == user);
+    var card = "<div class=\"card h-100\">";
+    if (comment.author.username === user) {
+        card += "<div class=\"dropdown\">\n        <button class=\"btn dropdown-toggle\" type=\"button\" id=\"dropdownMenuButton-" + comment.reply.id + "\"\n          data-bs-toggle=\"dropdown\" aria-expanded=\"false\">\n          ...\n        </button>\n        <ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuButton-" + comment.reply.id + "\">\n          <li><button id=\"delete-post-" + comment.reply.id + "\" data-bs-toggle=\"modal\"\n              data-bs-target=\"#delete-modal-" + comment.reply.id + "\" class=\"dropdown-item\">Delete </button></li>\n        </ul>\n      </div>\n\n      <div class=\"modal fade\" id=\"delete-modal-" + comment.reply.id + "\" tabindex=\"-1\"\n        aria-labelledby=\"modal-label-" + comment.reply.id + "\" aria-hidden=\"true\">\n        <div class=\"modal-dialog\">\n          <div class=\"modal-content\">\n            <div class=\"modal-header\">\n              <h5 class=\"modal-title\" id=\"modal-label-" + comment.reply.id + "\">Are you sure?</h5>\n              <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button>\n            </div>\n            <div class=\"modal-body\">\n              Once the answer is deleted, can never be restored. Do you want to continue?\n            </div>\n            <div class=\"modal-footer\">\n              <button type=\"button\" class=\"btn btn-success\" data-bs-dismiss=\"modal\">No, Cancel</button>\n              <a type=\"button\" class=\"btn btn-danger\" href=\"/post/delete_answer/" + comment.reply.id + "\">Yes, Delete\n                it</a>\n            </div>\n          </div>\n        </div>\n      </div>";
+    }
+    card += "<div class=\"container-fluid\">\n        <div class=\"card-footer row\">\n          <div class=\"col\">\n            <small> @" + comment.author.username + " comments:</small>\n          </div>\n          <div class=\"col d-flex flex-row-reverse\">";
+    if (comment.reply.already_voted) {
+        card += " <i class=\"fas fa-check-circle\" id = \"answer-" + comment.reply.id + "\" title = \"Remove Support\"\n              onclick=\"voteAnswer(" + comment.reply.id + ")\"></i>";
+    }
+    else {
+        card += "<i class=\"fas fa-check\" id = \"answer-" + comment.reply.id + "\" title = \"Support\"\n  onclick = \"voteAnswer(" + comment.reply.id + ")\" >\n    </i>\n  ";
+    }
+    card +=
+        "&nbsp; &nbsp; &nbsp;\n    <i class=\"far fa-flag\" id = \"report-answer-" + comment.reply.id + "\" data-bs-toggle=\"modal\"\n  data-bs-target=\"#report-modal-" + comment.reply.id + "\" onclick = \"reportAnswer(" + comment.reply.id + ")\" > </i>\n      <div class= \"modal fade\" id = \"report-modal-" + comment.reply.id + "\" tabindex=\"-1\" aria-labelledby=\"modal-label-" + comment.reply.id + "\" aria-hidden=\"true\" >\n    <div class=\"modal-dialog\">\n      <div class=\"modal-content\">\n        <div class=\"modal-header\">\n          <h5 class=\"modal-title\" id=\"modal-label-" + comment.reply.id + "\"> Comment was reported!</h5>\n            <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button>\n              </div>\n              <div class=\"modal-body\">\n                We'll check this post and if it is against our policy, we will remove it. Thank you so much\n                  </div>\n                  <div class=\"modal-footer\">\n                    <button type=\"button\" class=\"btn btn-primary\" data-bs-dismiss=\"modal\"> Okay </button>\n                      </div>\n                      </div>\n                      </div>\n                      </div>\n                        </div>\n                        </div>\n                        </div>\n                          ";
+    var date_obj = new Date(comment.reply.posted_on);
+    card += "<div class=\"card-body\">\n        <p class=\"card-text\"> " + comment.reply.text + " </a></p>\n        <small class=\"text-secondary\"> On\n          <time datetime=\"" + comment.reply.posted_on + "|date:'c'}\"\n            id=\"time-" + comment.reply.id + "\">" + (date_obj.toDateString().toString().slice(4) + " at " + date_obj.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })) + "</time>\n        </small>\n        &nbsp; &nbsp; &nbsp;\n      </div>";
+    return card;
+};
+var setPageNumber = function (answer_id, page_number, user) {
+    var button = document.getElementById("load-replies-button-" + answer_id);
+    button.setAttribute('onClick', "fetchComment(" + answer_id + "," + page_number + ",'" + user + "')");
+};
+var fetchComment = function (answer_id, page_number, user) { return __awaiter(_this, void 0, void 0, function () {
+    var response, data, parent_element, load_button, _i, data_1, comment;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, makeRequest("/api/fetch_answer_replies/" + answer_id + "/" + page_number)];
+            case 1:
+                response = _a.sent();
+                data = response.data;
+                parent_element = document.getElementById("load-replies-" + answer_id);
+                if (Object.keys(data).length === 0) {
+                    load_button = document.getElementById("load-replies-button-" + answer_id);
+                    load_button.remove();
+                }
+                else {
+                    for (_i = 0, data_1 = data; _i < data_1.length; _i++) {
+                        comment = data_1[_i];
+                        console.log(comment);
+                        parent_element.insertAdjacentHTML('afterbegin', createReplyCard(comment, user));
+                    }
+                }
+                setPageNumber(answer_id, page_number + 1, user);
+                return [2 /*return*/];
+        }
+    });
+}); };
 document.addEventListener('DOMContentLoaded', function () {
     var elements = document.querySelectorAll('time');
     elements.forEach(function (element) {
