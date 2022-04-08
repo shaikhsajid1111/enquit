92562955ddfcfa748777e06c2122fc6ebc523b59
Akankshi gupta <akankshigp12345@gmail.com>
2022-04-08 15:31:00 +0530

    change environment from production to development, added required in the reply

--- enquit/settings.py
+++ enquit/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
